d1a3440386ea7661e51dab97861ffb821de55a80
selimdawa <selimdawa@gmail.com>
2026-04-10 02:35:10 +0300

    ADD minorApiLevel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,9 @@ plugins {
 android {
     namespace = "com.flatcode.littletasks"
     compileSdk {
-        version = release(36)
+        version = release(36) {
+            minorApiLevel = 1
+        }
     }
 
     defaultConfig {
