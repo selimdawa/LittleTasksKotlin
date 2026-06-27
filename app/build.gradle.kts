f2fdbb5688252ec1077c84a5c926703f2b3ce49a
selimdawa <selimdawa@gmail.com>
2026-06-27 04:17:04 +0300

    KSP DEL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.google.services)
-    alias(libs.plugins.google.ksp)
 }
 
 android {
@@ -49,11 +48,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-        }
-    }
     buildFeatures {
         dataBinding = true
     }
@@ -70,6 +64,11 @@ dependencies {
     //Layout
     implementation(libs.androidx.constraintlayout)
     implementation(libs.material)
+    //Image
+    implementation(libs.circleimageview)                //Circle Image
+    implementation(libs.glide)                          //Glide Image
+    implementation(libs.glide.transformations)          //Glide Image Blur
+    api(libs.android.image.cropper)                     //Image Crop
     //Firebase
     implementation(platform(libs.firebase.bom)) //Firebase BOM
     implementation(libs.firebase.auth)
@@ -77,14 +76,7 @@ dependencies {
     implementation(libs.firebase.storage)
     implementation(libs.firebase.analytics)
     //implementation(libs.firebase.crashlytics)
-    //Image
-    implementation(libs.circleimageview)                //Circle Image
-    implementation(libs.glide)                          //Glide Image
-    ksp(libs.glide.ksp)                                 //Glide Compiler
-    implementation(libs.material.ripple)                //Ripple Effect
-    api(libs.android.image.cropper)                     //Image Crop
-    implementation(libs.glide.transformations)          //Image Blur
     //Other's
     implementation(libs.nafisbottomnav)                 //Bottom Navigation
-    ksp(libs.kotlin.metadata.jvm)                       //Kotlin
+    implementation(libs.material.ripple)                //Ripple Effect
 }
